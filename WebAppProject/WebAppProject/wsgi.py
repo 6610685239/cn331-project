19b3278a19afe3d576b2a33b9aeb1eb09b022ffd
Parunchai Timklip <6610685239@cn103>
2024-11-09 22:26:33 +0700

    requirements.txt

--- WebAppProject/WebAppProject/wsgi.py
+++ WebAppProject/WebAppProject/wsgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 import sys
 
-sys.path.append("/home/tutalk/cn331-project")
-sys.path.append("/home/tutalk/cn331-project/WebAppProject/WebAppProject")
 
 from django.core.wsgi import get_wsgi_application
 
