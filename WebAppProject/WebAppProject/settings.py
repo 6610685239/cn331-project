c5bb01df6c94479647ca4b227e81903ca506c1e0
Parunchai Timklip <6610685239@cn103>
2024-11-10 00:11:18 +0700

    static

--- WebAppProject/WebAppProject/settings.py
+++ WebAppProject/WebAppProject/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-
+STATIC_ROOT = "/home/cn331-project/WebAppProject/static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
