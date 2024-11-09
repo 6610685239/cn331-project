98be656b309fee8fb43a9a18d6d3562b5abcf87f
Parunchai Timklip <6610685239@cn103>
2024-11-09 08:28:26 +0700

    change WSGI

--- WebAppProject/WebAppProject/settings.py
+++ WebAppProject/WebAppProject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-6_c&)bsb_n6ft+h0ikl509b1zrf4l9xjqib=mry3v&d^#ho4+s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["tutalk.pythonanywhere.com"]
 
