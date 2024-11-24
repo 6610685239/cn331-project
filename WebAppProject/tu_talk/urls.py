277f2ba83a56850a0ef49fda6e709d163a0ee955
Chayawat Kanjanakaew <6610685122@cn103>
2024-11-24 16:42:28 +0700

    debug navbar can go to where its written & remove unknown line in party_list

--- WebAppProject/tu_talk/urls.py
+++ WebAppProject/tu_talk/urls.py
@@ -9,9 +9,6 @@ urlpatterns = [
     path("post/new/", views.create_post, name="create_post"),
     path("post/<int:post_id>/like/", views.like_post, name="like_post"),
     path("post/<int:post_id>/comment/", views.add_comment, name="add_comment"),
-    path(
-        "post/<int:post_id>/comments/",
-        views.view_all_comments,
-        name="view_all_comments",
+    path("post/<int:post_id>/comments/",views.view_all_comments,name="view_all_comments",
     ),
 ]
